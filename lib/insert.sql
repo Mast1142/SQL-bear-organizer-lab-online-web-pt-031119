18a06bb18826fd769929f33557007e2441318bf1
Samuel T <ssamt1142@gmail.com>
2019-05-26 04:22:07 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,127 @@
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  'Mr.Chocolate',
+  25,
+  'Male',
+  'Dark Brown',
+  'Sweet',
+  TRUE
+);
+
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  'Rowdy',
+  25,
+  'Female',
+  'Dark Brown',
+  'Rowdy',
+  TRUE
+);
+
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  'Tabitha',
+  28,
+  'Female',
+  'Golden',
+  'Ferocious',
+  TRUE
+);
+
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  'Sergeant Brown',
+  18,
+  'Male',
+  'Super Brown',
+  'Feral',
+  TRUE
+);
+
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  'Melissa',
+  22,
+  'Female',
+  'Golden',
+  'Timid',
+  False
+);
+
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  'Grinch',
+  28,
+  'Female',
+  'Green',
+  'Slow',
+  TRUE
+);
+
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  'Wendy',
+  73,
+  'Female',
+  'Silver',
+  'Protective',
+  TRUE
+);
+
+INSERT INTO bears(
+  name, 
+  age, 
+  gender,
+  color,
+  temperament,
+  alive,
+) VALUES(
+  NULL,
+  80,
+  'Male',
+  'Shiny',
+  'Ferocious',
+  TRUE
+);
